edb7ddd4e3e5a6a3ffb6383e1a2ed809add43f87
abin0x <mahmudulabin@gmail.com>
2024-09-23 22:47:00 +0600

    login

--- users/views.py
+++ users/views.py
@@ -70,7 +70,7 @@ def activate(request, uid64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return redirect('https://onlinescl.netlify.app/login.html')
+        return redirect('https://coursescl.netlify.app/login.html')
     else:
         return redirect('register')
 
